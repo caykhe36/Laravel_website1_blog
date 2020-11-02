fd83cf72c96cc84128765cb8135465d91652c92c
caykhe36 <Anhbinhvipboy123@>
2020-11-02 21:18:46 +0700

    Lam giao dien login

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'Home@index');      
+Route::get('/', function () {
+    return view('home/index');
+});
+Route::get('/login', function () {
+    return view('login/index');
+});
